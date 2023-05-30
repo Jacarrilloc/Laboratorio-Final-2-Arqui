1f989bbeaf99470ed115a0c4bab5544ecde7c61e
Julian Carrillo <78111031+Jacarrilloc@users.noreply.github.com>
2023-05-29 23:28:32 -0500

    Correcciones

--- hexa-spring-boot-app/scripts/persona_ddl_maria.sql
+++ hexa-spring-boot-app/scripts/persona_ddl_maria.sql
@@ -1,49 +1,44 @@
-FLUSH PRIVILEGES;
---
-DROP USER IF EXISTS 'persona_db'@'%';
-DROP SCHEMA IF EXISTS `persona_db`;
---
-CREATE USER IF NOT EXISTS 'persona_db'@'%' IDENTIFIED BY 'persona_db';
-CREATE SCHEMA IF NOT EXISTS `persona_db`; 
---
-GRANT EXECUTE, TRIGGER, INSERT, UPDATE, DELETE, SELECT ON `persona_db`.* TO 'persona_db'@'%'; 
-FLUSH PRIVILEGES; 
---
-USE `persona_db`;
---
-CREATE TABLE IF NOT EXISTS `persona_db`.`persona` (
- `cc` INT(15) NOT NULL,
- `nombre` VARCHAR(45) NOT NULL,
- `apellido` VARCHAR(45) NOT NULL,
- `genero` ENUM('M', 'F') NOT NULL,
- `edad` INT(3) NULL DEFAULT NULL, 
- CONSTRAINT `persona_pk` PRIMARY KEY (`cc`)
-);
---
-CREATE TABLE IF NOT EXISTS `persona_db`.`profesion` (
- `id` INT(6) NOT NULL,
- `nom` VARCHAR(90) NOT NULL,
- `des` TEXT NULL DEFAULT NULL, 
- CONSTRAINT `profesion_pk` PRIMARY KEY (`id`)
-);
---
-CREATE TABLE IF NOT EXISTS `persona_db`.`telefono` (
- `num` VARCHAR(15) NOT NULL,
- `oper` VARCHAR(45) NOT NULL,
- `duenio` INT(15) NOT NULL, 
- CONSTRAINT `telefono_pk` PRIMARY KEY (`num`), 
- CONSTRAINT `telefono_persona_fk` FOREIGN KEY (`duenio`) REFERENCES `persona_db`.`persona` (`cc`)
-);
---
-CREATE TABLE IF NOT EXISTS `persona_db`.`estudios` (
- `id_prof` INT(6) NOT NULL,
- `cc_per` INT(15) NOT NULL,
- `fecha` DATE NULL DEFAULT NULL,
- `univer` VARCHAR(50) NULL DEFAULT NULL, 
- CONSTRAINT `estudios_pk` PRIMARY KEY (`id_prof`, `cc_per`),
- CONSTRAINT `estudio_persona_fk` FOREIGN KEY (`cc_per`) REFERENCES `persona_db`.`persona` (`cc`), 
- CONSTRAINT `estudio_profesion_fk` FOREIGN KEY (`id_prof`) REFERENCES `persona_db`.`profesion` (`id`)
-);
---
-COMMIT;
-FLUSH PRIVILEGES;
+-- Crear la base de datos
+CREATE DATABASE IF NOT EXISTS persona_db;
+USE persona_db;
+
+-- Tabla persona
+CREATE TABLE IF NOT EXISTS persona (
+                                       cc INT NOT NULL PRIMARY KEY,
+                                       nombre VARCHAR(45) NOT NULL,
+    apellido VARCHAR(45) NOT NULL,
+    genero CHAR(1) NOT NULL,
+    edad INT NULL
+    );
+
+-- Tabla profesion
+CREATE TABLE IF NOT EXISTS profesion (
+                                         id INT NOT NULL PRIMARY KEY,
+                                         nom VARCHAR(90) NOT NULL,
+    des TEXT NULL
+    );
+
+-- Tabla estudios
+CREATE TABLE IF NOT EXISTS estudios (
+                                        id_prof INT NOT NULL,
+                                        cc_per INT NOT NULL,
+                                        fecha DATE NULL,
+                                        univer VARCHAR(50) NULL,
+    PRIMARY KEY (id_prof, cc_per),
+    CONSTRAINT fk_estudios_persona
+    FOREIGN KEY (cc_per)
+    REFERENCES persona (cc),
+    CONSTRAINT fk_estudios_profesion
+    FOREIGN KEY (id_prof)
+    REFERENCES profesion (id)
+    );
+
+-- Tabla telefono
+CREATE TABLE IF NOT EXISTS telefono (
+    num VARCHAR(15) NOT NULL PRIMARY KEY,
+    oper VARCHAR(45) NOT NULL,
+    duenio INT NOT NULL,
+    CONSTRAINT fk_telefono_persona
+    FOREIGN KEY (duenio)
+    REFERENCES persona (cc)
+    );
